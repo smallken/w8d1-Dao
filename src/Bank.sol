be96e1798cd446bc8b439c3f2805ebc70092a419
smallken <4125902@qq.com>
2024-03-12 11:41:54 +0800

    sth done

--- src/Bank.sol
+++ src/Bank.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Bank {
-    
+    /**
+     * 按我的理解，实际上就是
+     */
+    string public constant name = "Dragon";
+    string public constant symbol = "DRG";
+    uint public constant totalSupply = 10000000e18; // 10 million 
+    mapping (address => uint96) internal balances;
+    constructor (address deployer){
+        balances[deployer] = uint96(totalSupply);
+    }
+
+    function withdrawByGovernor() public {
+        
+    }
+
+
+
 }
